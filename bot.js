5b35ef52ab53c43dcdf40b08dad5ee470e9574b5
nerkverse <43081110+nerkverse@users.noreply.github.com>
2018-09-08 13:33:05 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+{
+  "ClientId": 487737409388281856,
+  "Token": "NDg3NzM3NDA5Mzg4MjgxODU2.DnSIOw.pjGv8LV-JVfokrU4DjOYFpja9Zc",
+  "OwnerIds": [
+    176765417765732352,
+	160922456872189952,
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyCpU8CAgTnel5GTCGq6HAZx7g7Po35J3RE",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null,
+  "RedisOptions": null
+}
